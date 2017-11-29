3bc1c1543171528aa70651c0b4db1a9259e0b06b
dawsonrichey <dawsonrichey89@gmail.com>
2017-11-28 20:33:24 -0500

    fixed break text now displayed

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MessagesComponent } from './messages.component';
+import { MessagesComponent } from './messages.component'
 
 
 @NgModule({
