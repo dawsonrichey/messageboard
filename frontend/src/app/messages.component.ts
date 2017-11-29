6beeb3556e50d2da816e2ab98eb79714b9b009c7
dawsonrichey <dawsonrichey89@gmail.com>
2017-11-29 01:06:44 -0500

    array of temp info now displayed

--- frontend/src/app/messages.component.ts
+++ frontend/src/app/messages.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'messages',
-    template: 'this is the messages component'
+    template: 'this is the messages component<div *ngFor="let message of messages">{{message.text}} by: {{message.owner}}</div>'
 })
 export class MessagesComponent {
-
+    messages = [{text:'this is text', owner:'Jane'}, {text:'other text', owner:'Joe'}];
 }
