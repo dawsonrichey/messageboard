f0c9975d7bbc76bc81db5e42f484aaf17850df94
dawsonrichey <dawsonrichey89@gmail.com>
2017-11-29 02:43:45 -0500

    running nodemon with server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello');
+var messages = [{text:'this is text', owner:'Jane'}, {text:'other text', owner:'Joe'}];
+
+app.get('/messages', (req, res) => {
+    res.json(messages);
 })
 
 app.listen(1234);
