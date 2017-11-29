9e7c101b4024c205858b63ccc8010dd87d160d2d
dawsonrichey <dawsonrichey89@gmail.com>
2017-11-29 02:11:42 -0500

    Added Material design to the app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { MessagesComponent } from './messages.component'
@@ -10,7 +12,7 @@ import { MessagesComponent } from './messages.component'
     AppComponent, MessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
